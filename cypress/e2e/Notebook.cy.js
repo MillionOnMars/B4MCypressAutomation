04e762d9508d3579cace049545844c3579b92a44
MattTan257 <matt@milliononmars.com>
2025-05-05 15:21:11 +0800

    Verify File Operations

--- cypress/e2e/Notebook.cy.js
+++ cypress/e2e/Notebook.cy.js
@@ -6,7 +6,7 @@ describe('Notebook Operations', () => {
     // Log in to the application before running the tests
     
     beforeEach(() => {login('wescarda5', 'Password12345!');});
-    // Choose 3 random text models and create a new notebook
+    //Choose 3 random text models and create a new notebook
     randomTextModels.forEach((model) => {
         Notebook.createNotebook('List down the top 5 prime numbers from the smallest', model, 'cypress/fixtures/upload/cat.png');
     });
@@ -14,4 +14,6 @@ describe('Notebook Operations', () => {
     Notebook.renameNotebook('Renamed Notebook');
     // Deletes the notebook
     Notebook.deleteNotebook('Renamed Notebook');
+    //add a file to the notebook
+    Notebook.Files('cypress/fixtures/upload/cat.png');
 });
