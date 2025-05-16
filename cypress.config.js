af2d9e8117f83f66db5a7def014616a521a5c084
Wil <wilfred.escarda@gmail.com>
2025-05-16 14:58:16 +0800

    Added env variable

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,21 @@
 const { defineConfig } = require("cypress");
+const prod = 'https://app.bike4mind.com/';
+const staging = 'https://app.staging.bike4mind.com/';
+
 
 module.exports = defineConfig({
   e2e: {    
     specPattern: [
-      // 'cypress/e2e/Auth.cy.js',
+      'cypress/e2e/Auth.cy.js',
       // 'cypress/e2e/Signup.cy.js',
       // 'cypress/e2e/Projects.cy.js',
-      // 'cypress/e2e/Notebook.cy.js'
-      // 'cypress/e2e/30Prompts.cy.js',
-      'cypress/e2e/30Prompts.cy.js'
+      // 'cypress/e2e/Notebook.cy.js',
+      // 'cypress/e2e/30Prompts.cy.js'
     ],
     supportFile: 'cypress/support/index.js',
+    env: {// <-- Set your global app URL here
+      appUrl: staging // <-- Set your global URL here
+    },
     setupNodeEvents(on, config) {
       on('after:spec', (spec, results) => {
         // You can add logic here if needed
