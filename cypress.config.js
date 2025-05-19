e347ecad1e891908e2ea5f4bf8163b6096dbd6fa
Wil <wilfred.escarda@gmail.com>
2025-05-19 11:22:59 +0800

    Add GitHub Actions workflow for Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
 const { defineConfig } = require("cypress");
 
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
-
 module.exports = defineConfig({
-  e2e: {
-      supportFile: 'cypress/support/index.js', // Ensure this path is correct
+  e2e: {    
+    specPattern: [
+      'cypress/e2e/Auth.cy.js',
+      'cypress/e2e/Signup.cy.js',
+      'cypress/e2e/Projects.cy.js',
+      'cypress/e2e/Notebook.cy.js',
+      'cypress/e2e/30Prompts.cy.js'
+    ],
+    supportFile: 'cypress/support/index.js',
+    env: {
+      appUrl: process.env.CYPRESS_APP_URL || 'https://app.staging.bike4mind.com/'
+    },
   },
 });
