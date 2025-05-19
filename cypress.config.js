e11d98dbbc8bc8cbf92f33295da705097e83317f
Wil <wilfred.escarda@gmail.com>
2025-05-19 12:06:55 +0800

    Cypress Run

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {    
     specPattern: [
-      'cypress/e2e/Auth.cy.js',
-      'cypress/e2e/Signup.cy.js',
-      'cypress/e2e/Projects.cy.js',
-      'cypress/e2e/Notebook.cy.js',
-      'cypress/e2e/30Prompts.cy.js'
+      'cypress/e2e/Auth.cy.js'
+      // 'cypress/e2e/Signup.cy.js',
+      // 'cypress/e2e/Projects.cy.js',
+      // 'cypress/e2e/Notebook.cy.js',
+      // 'cypress/e2e/30Prompts.cy.js'
     ],
     supportFile: 'cypress/support/index.js',
     env: {
