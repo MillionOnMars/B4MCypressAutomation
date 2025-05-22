2f098cf9ea3b58c74aad4a65b31ae7b714e405b2
Wil <wilfred.escarda@gmail.com>
2025-05-22 11:57:58 +0800

    Updated upload selector

--- cypress/support/Notebook.js
+++ cypress/support/Notebook.js
@@ -193,9 +193,11 @@ const selectTxtModel = (model) => {
         .click({ force: true });
 
     //clicks close button
-    cy.get('.MuiBox-root.css-f0am11 > button')
-        .should('be.visible')
-        .click();
+    if(model !== 'gpt-4.1'){
+        cy.get('.MuiBox-root.css-f0am11 > button')
+            .should('be.visible')
+            .click();
+    }
 }
 
 
