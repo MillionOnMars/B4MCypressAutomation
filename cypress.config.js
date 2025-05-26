2adb72c36b13f720871e92cc2dc6aa67e4ff777e
Wil <wilfred.escarda@gmail.com>
2025-05-26 14:56:17 +0800

    Updated list

--- cypress.config.js
+++ cypress.config.js
@@ -11,11 +11,11 @@ const environments = {
 module.exports = defineConfig({
   e2e: {    
     specPattern: [
-      'cypress/e2e/Auth.cy.js',
-      'cypress/e2e/Signup.cy.js',
-      'cypress/e2e/Projects.cy.js',
-      'cypress/e2e/Notebook.cy.js',
-      'cypress/e2e/30Prompts.cy.js'
+      'cypress/e2e/Auth.cy.js'
+      // 'cypress/e2e/Signup.cy.js',
+      // 'cypress/e2e/Projects.cy.js',
+      // 'cypress/e2e/Notebook.cy.js',
+      // 'cypress/e2e/30Prompts.cy.js'
     ],
     supportFile: 'cypress/support/index.js',
     env: {
